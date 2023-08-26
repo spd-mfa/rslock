5ee27037b55fc1c6d057ef04608a02721d517fc8
Henry Zimmerman <hzimmerm@akamai.com>
2023-08-26 06:13:49 -0400

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(any(feature = "async-std", feature = "tokio"))]
 mod lock;
 
-#[cfg(any(feature = "async-std", feature = "tokio"))]
-pub use crate::lock::{Lock, LockError, LockManager};
 #[cfg(all(feature = "async-std", not(feature = "tokio")))]
 pub use crate::lock::LockGuard;
+#[cfg(any(feature = "async-std", feature = "tokio"))]
+pub use crate::lock::{Lock, LockError, LockManager};
